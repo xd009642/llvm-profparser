8d370839c14ed9671142813c93b3442e4f4a65f1
xd009642 <danielmckenna93@gmail.com>
2021-03-19 14:57:25 +0400

    Add default cutoffs

--- src/summary.rs
+++ src/summary.rs
@@ -1,7 +1,23 @@
+/// The amount to scale cutoffs by to go back to a more readable percentile
+pub const CUTOFF_SCALE: u64 = 10_000_000;
+
+/// These are the default cutoffs for profile summary entries. Without cutoffs specified manually
+/// llvm-profdata and associated tools will use these. These numbers represent percentiles of
+/// counts in the profile data scaled by 10_000_000 (divide by these scales to go back to
+/// percentiles)
+pub const DEFAULT_CUTOFFS: [u64; 16] = [
+    10000, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 950000, 990000,
+    999000, 999900, 999990, 999999,
+];
+
+/// The type of the profile summary
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Kind {
+    /// Instrumentation profile
     Instr,
+    /// Context Sensitive Instrumentation profile
     CsInstr,
+    /// Sample based profile
     Sample,
 }
 
