4218e1f348b8998e0ae9eba7e9dbf12b4e90e9c3
xd009642 <danielmckenna93@gmail.com>
2021-03-19 21:57:55 +0400

    Extract number of buckets and entries

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -1,4 +1,4 @@
-use nom::IResult;
+use nom::{number::complete::*, IResult};
 
 pub(crate) struct HashTable {}
 
@@ -7,12 +7,15 @@ impl HashTable {
     /// hash. offset shows the offset from the base address to the start of the `HashTable` as this
     /// will be used to correct any offsets
     pub(crate) fn parse(input: &[u8], offset: usize, bucket_start: usize) -> IResult<&[u8], Self> {
+        assert!(bucket_start > 0);
         println!("Table starts at: {}", offset);
         println!(
             "Buckets start at {} or {}",
             bucket_start,
             offset + bucket_start
         );
+        let (bytes, num_buckets) = le_u64(&input[bucket_start..])?;
+        let (bytes, num_entries) = le_u64(bytes)?;
         todo!()
     }
 }
