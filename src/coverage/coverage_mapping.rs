b7a3031b6a9db04a7e249ecc7eb462f0d8ee7586
xd009642 <danielmckenna93@gmail.com>
2021-07-31 17:19:56 +0100

    Add Object and start parsing covering mapping

--- src/coverage/coverage_mapping.rs
+++ src/coverage/coverage_mapping.rs
@@ -1,13 +1,56 @@
 use crate::instrumentation_profile::types::*;
-use std::path::PathBuf;
+use crate::util::*;
+use object::{Endian, Endianness, Object, ObjectSection, Section};
+use std::convert::TryInto;
+use std::error::Error;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 pub struct CoverageMapping<'a> {
     profile: &'a InstrumentationProfile,
-    //objects:
 }
 
 impl<'a> CoverageMapping<'a> {
-    pub fn new(_objects: &[PathBuf], profile: &'a InstrumentationProfile) -> Self {
-        Self { profile }
+    pub fn new(
+        object_files: &[PathBuf],
+        profile: &'a InstrumentationProfile,
+    ) -> Result<Self, Box<dyn Error>> {
+        for object in object_files {
+            let binary_data = fs::read(object)?;
+            let object_file = object::File::parse(&*binary_data)?;
+            let covmap = object_file
+                .section_by_name("__llvm_covmap")
+                .or(object_file.section_by_name(".lcovmap$M"))
+                .map(|x| parse_coverage_mapping(object_file.endianness(), &x));
+        }
+
+        todo!()
+    }
+}
+
+fn parse_coverage_mapping<'data, 'file>(endian: Endianness, section: &Section<'data, 'file>) {
+    if let Ok(data) = section.data() {
+        println!("Length: {}", data.len());
+        debug_assert_eq!(endian.read_i32_bytes(data[0..4].try_into().unwrap()), 0);
+        let filename_data_len = endian.read_i32_bytes(data[4..8].try_into().unwrap());
+        debug_assert_eq!(endian.read_i32_bytes(data[8..12].try_into().unwrap()), 0);
+        let format_version = endian.read_i32_bytes(data[12..16].try_into().unwrap());
+        println!(
+            "Filename len {} format_version {}",
+            filename_data_len, format_version
+        );
+
+        let bytes = &data[16..(16 + filename_data_len as usize)];
+        let (bytes, file_count) = parse_leb128(bytes).unwrap();
+        let (bytes, string) = parse_string_ref(bytes).unwrap();
+        //let (bytes, length_of_filenames) = parse_leb128(bytes).unwrap();
+        //let (mut bytes, compressed_length) = parse_leb128(bytes).unwrap();
+
+        println!(
+            "Filecount {} remaining: {}\n string: {}",
+            file_count,
+            bytes.len(),
+            string.trim()
+        );
     }
 }
