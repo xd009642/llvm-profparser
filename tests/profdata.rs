8da7439b4d45537b52cf3c38af4aa6c37e9087a1
xd9642 <danielmckenna93@gmail.com>
2022-02-19 17:47:58 +0000

    Add llvm-14 test corpora
    Minus the overflow tests because I don't really care about that functionality

--- tests/profdata.rs
+++ tests/profdata.rs
@@ -21,7 +21,12 @@ fn get_data_dir() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/data/profdata/llvm-13")
 }
 
-#[cfg(not(any(llvm_11, llvm_12, llvm_13)))]
+#[cfg(llvm_14)]
+fn get_data_dir() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/data/profdata/llvm-14")
+}
+
+#[cfg(not(any(llvm_11, llvm_12, llvm_13, llvm_14)))]
 fn get_data_dir() -> PathBuf {
     // Nothing to do so lets get a directory with nothing in
     PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/data/profdata")
