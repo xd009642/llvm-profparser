cfbb0aaf761d1e1d45d3f0124958d04c12b34003
xd9642 <danielmckenna93@gmail.com>
2024-05-15 22:41:10 +0100

    Move to a predicate

--- src/coverage/coverage_mapping.rs
+++ src/coverage/coverage_mapping.rs
@@ -5,7 +5,6 @@ use crate::util::*;
 use anyhow::{bail, Result};
 use nom::error::Error as NomError;
 use object::{Endian, Endianness, Object, ObjectSection, Section};
-use std::collections::HashSet;
 use std::convert::TryInto;
 use std::error::Error;
 use std::fmt;
@@ -150,19 +149,17 @@ impl<'a> CoverageMapping<'a> {
         result
     }
 
-    pub fn generate_subreport(&self, covered_files: Option<HashSet<PathBuf>>) -> CoverageReport {
+    pub fn generate_subreport<P>(&self, mut predicate: P) -> CoverageReport
+    where
+        P: FnMut(&[PathBuf]) -> bool,
+    {
         let mut report = CoverageReport::default();
         //let base_region_ids = info.get_simple_counters(self.profile);
         for info in &self.mapping_info {
             for func in &info.cov_fun {
                 let base_region_ids = self.get_simple_counters(func);
                 let paths = info.get_files_from_id(func.header.filenames_ref);
-                if paths.is_empty()
-                    || !covered_files
-                        .as_ref()
-                        .map(|covered| paths.iter().any(|file| covered.contains(file.as_path())))
-                        .unwrap_or(true)
-                {
+                if paths.is_empty() || !predicate(&paths) {
                     continue;
                 }
 
@@ -273,7 +270,7 @@ impl<'a> CoverageMapping<'a> {
     }
 
     pub fn generate_report(&self) -> CoverageReport {
-        self.generate_subreport(None)
+        self.generate_subreport(|_| true)
     }
 }
 
